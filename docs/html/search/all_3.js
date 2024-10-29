630a319e9f5641fefbcae745046ccc1d5e50061c
Robert Rostohar <robert.rostohar@arm.com>
2024-10-29 12:16:44 +0100

    Remove pre-generated documentation

--- docs/html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['using_20iot_20socket_0',['Using IoT Socket',['../iot_socket_using.html',1,'']]]
-];
--- docs/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['iotsocketapi_5ft_0',['iotSocketApi_t',['../group__iotSocketMux.html#structiotSocketApi__t',1,'']]]
-];
